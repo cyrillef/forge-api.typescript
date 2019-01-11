2cc69c612f8636fcc58d93f854ab0c7712a95066
dukedhx <dukedhx@hotmail.com>
2019-01-11 10:48:32 +0800

    added index, conventionalized README, fixed threejs reference

--- src/viewer.d.ts
+++ src/viewer.d.ts
@@ -14,7 +14,7 @@
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 //
-/// <reference path='../globals/three/index.d.ts' />
+/// <reference path='../../three/index.d.ts' />
 
 declare module Autodesk {
     export module Viewing {
